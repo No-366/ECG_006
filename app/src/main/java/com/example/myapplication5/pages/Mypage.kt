8f2c8685a339b43e763a532ebe9d77d53a7d85e7
woong <woongcheol99@naver.com>
2023-09-10 22:23:24 +0900

    mypage

---
+++ app/src/main/java/com/example/myapplication5/pages/Mypage.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication5.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication5.R
+import com.example.myapplication5.databinding.ActivityMypageBinding
+
+class Mypage : AppCompatActivity() {
+    private lateinit var binding: ActivityMypageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+
+        // 뷰 바인딩 초기화
+        binding = ActivityMypageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val namedata = intent.getStringExtra("namedata")
+        binding.textView2.text = "안녕하세요, "+namedata+"님"
+    }
+}
