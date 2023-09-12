fce8af2f92f026dde36e3d2e3d87f63fb9651ac8
woong <woongcheol99@naver.com>
2023-09-12 21:13:59 +0900

    Response_Model

---
+++ app/src/main/java/com/example/myapplication5/models/Response_Model.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication5.models
+
+data class Response_Model (
+
+    val token: String,
+    val type: String,
+    val refreshToken: String,
+    val id: String,
+    val username: String,
+    val email: String,
+    val school: String
+
+)
